6b4aad5fbfe6867a5990141740ad2f3c6360f44d
jiriLu <jirka.ludvik1@gmail.com>
2024-05-01 18:36:14 +0200

    user.controller cleanup

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,23 @@
-import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
-import { NotFoundError } from 'rxjs';
 
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) {}
 
     @Get('/:id')
-    async getUser(@Param('id') id: number){
+    async getUser(@Param('id') id: number): Promise<User> {
         return await this.userService.getUser(id);
     }
 
     @Get('/')
-    async getUsers(){
+    async getUsers(): Promise<User[]> {
         return await this.userService.getUsers();
     }
 
     @Post()
-    async createUser(@Body() user: User){
+    async createUser(@Body() user: User): Promise<User> {
         return await this.userService.createUser(user);
     }
 }
